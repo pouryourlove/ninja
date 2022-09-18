bcf0c9a3c665bda2b84f290dac41f41337ab3bb6
Juyeon Lee <lqu134@naver.com>
2022-09-18 22:30:40 +0900

    using previous state

--- src/App.js
+++ src/App.js
@@ -2,16 +2,32 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  const [name, setName] = useState("mario");
+  const [events, setEvents] = useState([
+    { title: "ju is the best", id: 1 },
+    { title: "hey Maisy", id: 2 },
+    { title: "Zzong is the best", id: 3 },
+  ]);
 
-  const handleClick = () => {
-    setName("juyeon");
-    console.log(name);
+  //Filter method에서 true면 내버려두고 false면 filter해줌..
+
+  const handleClick = (id) => {
+    setEvents((prevEvents) => {
+      return prevEvents.filter((event) => {
+        return id !== event.id;
+      });
+    });
+    console.log(id);
   };
   return (
     <div className="App">
-      <h1>My name is {name}</h1>
-      <button onClick={handleClick}>Change name</button>
+      {events.map((event, index) => (
+        <div key={event.id}>
+          <h2>
+            {index} - {event.title}
+          </h2>
+          <button onClick={() => handleClick(event.id)}>Delete event</button>
+        </div>
+      ))}
     </div>
   );
 }
