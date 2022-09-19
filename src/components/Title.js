08040d7d43847fba09edce945aa080a5a47928d6
Juyeon Lee <lqu134@naver.com>
2022-09-19 15:23:13 +0900

    creating a title component

---
+++ src/components/Title.js
@@ -0,0 +1,9 @@
+export default function Title() {
+  return (
+    <div>
+      <h1 className="title">J's events</h1>
+      <br />
+      <h2 className="subtitle">All the latest events in J's world</h2>
+    </div>
+  );
+}
