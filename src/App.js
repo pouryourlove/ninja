fd3a896ab56629c6674e421c6b6ea1fca93c76a3
Juyeon Lee <lqu134@naver.com>
2022-09-19 14:56:03 +0900

    conditional templates

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
+  const [showEvents, setShowEvents] = useState(true);
   const [events, setEvents] = useState([
     { title: "ju is the best", id: 1 },
     { title: "hey Maisy", id: 2 },
     { title: "Zzong is the best", id: 3 },
   ]);
+
+  console.log(showEvents);
 
   //Filter method에서 true면 내버려두고 false면 filter해줌..
 
@@ -20,14 +23,27 @@ function App() {
   };
   return (
     <div className="App">
-      {events.map((event, index) => (
-        <div key={event.id}>
-          <h2>
-            {index} - {event.title}
-          </h2>
-          <button onClick={() => handleClick(event.id)}>Delete event</button>
+      {showEvents && (
+        <div>
+          <button onClick={() => setShowEvents(false)}>hide events</button>
         </div>
-      ))}
+      )}
+
+      {!showEvents && (
+        <div>
+          <button onClick={() => setShowEvents(true)}>show events</button>
+        </div>
+      )}
+
+      {showEvents &&
+        events.map((event, index) => (
+          <div key={event.id}>
+            <h2>
+              {index} - {event.title}
+            </h2>
+            <button onClick={() => handleClick(event.id)}>Delete event</button>
+          </div>
+        ))}
     </div>
   );
 }
