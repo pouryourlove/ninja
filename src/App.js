911d319fdae74ccad573129b6a8f9d360c414c23
Juyeon Lee <lqu134@naver.com>
2022-09-22 18:15:24 +0900

    modal

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import "./App.css";
 import { useState } from "react";
 import Title from "./components/Title";
+import Modal from "./components/Modal";
+import EventList from "./components/EventList";
 
 function App() {
+  const [showModal, setShowModal] = useState(false);
   const [showEvents, setShowEvents] = useState(true);
   const [events, setEvents] = useState([
     { title: "ju is the best", id: 1 },
@@ -10,7 +13,7 @@ function App() {
     { title: "Zzong is the best", id: 3 },
   ]);
 
-  console.log(showEvents);
+  console.log(showModal);
 
   //Filter method에서 true면 내버려두고 false면 filter해줌..
 
@@ -23,33 +26,44 @@ function App() {
     console.log(id);
   };
 
+  const handleClose = () => {
+    setShowModal(false);
+  };
+
   const subtitle = "whatever..";
   return (
     <div className="App">
       <Title title="Events in Your Area" subtitle={subtitle} />
-      <Title title="another title" subtitle={"another subtitle"} />
-
       {showEvents && (
         <div>
           <button onClick={() => setShowEvents(false)}>hide events</button>
         </div>
       )}
-
       {!showEvents && (
         <div>
           <button onClick={() => setShowEvents(true)}>show events</button>
         </div>
       )}
-
-      {showEvents &&
-        events.map((event, index) => (
-          <div key={event.id}>
-            <h2>
-              {index} - {event.title}
-            </h2>
-            <button onClick={() => handleClick(event.id)}>Delete event</button>
-          </div>
-        ))}
+      {showEvents && <EventList events={events} handleClick={handleClick} />}
+      {/* <Modal>
+        <h2>10% Off Coupon</h2>
+        <p>Use the code NINJA10 at the checkout.</p>
+      </Modal> */}
+      {showModal && (
+        <Modal handleClose={handleClose}>
+          <h2>hello blahblah</h2>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime
+            corporis quas officia reiciendis nobis saepe excepturi tempore, quod
+            pariatur voluptatibus labore vel delectus animi, ipsum hic rerum
+            voluptatum quisquam earum.
+          </p>
+          <a href="#" className="href">
+            find out more...
+          </a>
+        </Modal>
+      )}
+      <button onClick={() => setShowModal(true)}>open</button>
     </div>
   );
 }
---
+++ src/components/Modal.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Modal.css";
+import ReactDOM from "react-dom";
+
+export default function Modal({ children, handleClose }) {
+  return ReactDOM.createPortal(
+    <div className="modal-backdrop">
+      <div className="modal">
+        {children}
+        <button onClick={handleClose}>close</button>
+      </div>
+    </div>,
+    document.body
+  );
+}
