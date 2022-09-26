2d2174f3b844073021ff027dd80b35cb8685ae9d
Juyeon Lee <lqu134@naver.com>
2022-09-26 17:41:41 +0900

    select box

--- src/components/NewEventForm.js
+++ src/components/NewEventForm.js
@@ -3,29 +3,32 @@ import React from "react";
 import { useState, useRef } from "react";
 
 export default function NewEventForm({ addEvent }) {
-  // const [title, setTitle] = useState("");
-  // const [date, setDate] = useState("");
-  const title = useRef();
-  const date = useRef();
+  const [title, setTitle] = useState("");
+  const [date, setDate] = useState("");
+  const [location, setLocation] = useState("Manchester");
 
   const resetForm = () => {
-    title.current.value = "";
-    date.current.value = "";
+    setTitle("");
+    setDate("");
+    setLocation("Manchester");
   };
 
-  // const handleChange = (e) => {
-  //   setTitle(e.target.value)
-  // };
+  const handleChange = (e) => {
+    setTitle(e.target.value);
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(title, date);
 
     const event = {
-      title: title.current.value,
-      date: date.current.value,
+      title: title,
+      date: date,
+      location: location,
       id: Math.floor(Math.random() * 10000),
     };
+
+    console.log(event);
 
     addEvent(event);
     resetForm();
@@ -35,11 +38,27 @@ export default function NewEventForm({ addEvent }) {
     <form className="new-event-form" onSubmit={handleSubmit}>
       <label>
         <span>Event Title:</span>
-        <input type="text" ref={title} />
+        <input
+          type="text"
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+        />
       </label>
       <label>
         <span>Event Date:</span>
-        <input type="date" ref={date} />
+        <input
+          type="date"
+          onChange={(e) => setDate(e.target.value)}
+          value={date}
+        />
+      </label>
+      <label>
+        <span>Event location:</span>
+        <select onChange={(e) => setLocation(e.target.value)}>
+          <option value="manchester">Manchester</option>
+          <option value="london">London</option>
+          <option value="cardiff">Cardiff</option>
+        </select>
       </label>
       <button>Submit</button>
     </form>
